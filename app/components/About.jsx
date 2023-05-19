f149e1018aee22540be504921df0aeeabe507453
filipkenkov <filip.kenkov@miraclemill.com>
2023-05-19 18:37:46 +0200

    about page done

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -7,14 +7,14 @@ const About = () => {
     <div id="about" className="w-full items-center p-2 md:h-screen">
       <div className='max-w-[1240px] m-auto md:grid grid-cols-3 gap-8'>
       <div className="col-span-2">
-          <p className="text-xl text-blue-800">About</p>
-          <h2 className="text-white">Who am I?</h2>
+          <p className="text-2xl font-bold text-blue-800 py-2">About</p>
+          <h2 className="text-white py-4">Who am I?</h2>
           <p className="text-white font-mono">
             A gradute on Faculty of Computer Science and Engineering with
             passion for computers since early age.
             <br /> I always knew that I would love to work with computers and
             eventually become a Software Developer. <br />
-            Naah, I didn't like hardware at all. I always watch youtube videos
+            Naah, I did not like hardware at all. I always watch youtube videos
             for sticking hardware components together. <br /> As you can see, I
             have a passion about web development, but I also like to develop
             mobile applications
