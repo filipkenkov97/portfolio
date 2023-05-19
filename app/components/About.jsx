f0a612a6e2f8346c8214c509d542c19ca7c33967
filipkenkov <filip.kenkov@miraclemill.com>
2023-05-19 18:32:21 +0200

    about done

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -1,9 +1,38 @@
-import React from 'react'
-
-export default function About() {
+import React from 'react';
+import Image from 'next/image';
+import Link from 'next/link';
+import AboutImg from '../../public/static/profile.jpg'
+const About = () => {
   return (
-    <div>
-      
+    <div id="about" className="w-full items-center p-2 md:h-screen">
+      <div className='max-w-[1240px] m-auto md:grid grid-cols-3 gap-8'>
+      <div className="col-span-2">
+          <p className="text-xl text-blue-800">About</p>
+          <h2 className="text-white">Who am I?</h2>
+          <p className="text-white font-mono">
+            A gradute on Faculty of Computer Science and Engineering with
+            passion for computers since early age.
+            <br /> I always knew that I would love to work with computers and
+            eventually become a Software Developer. <br />
+            Naah, I didn't like hardware at all. I always watch youtube videos
+            for sticking hardware components together. <br /> As you can see, I
+            have a passion about web development, but I also like to develop
+            mobile applications
+            <br />
+            So dig deep and find out more about me
+          </p>
+          
+        </div>
+        <div className="w-full h-auto m-auto shadow-xl shadow-gray-400 rounded-xl flex items-center justify-center p-4">
+            <Image src={AboutImg}
+            className="rounded-lg"
+              alt="/"
+            />
+          </div>
+      </div>
     </div>
-  )
-}
+  );
+};
+
+export default About;
+    
--- app/page.jsx
+++ app/page.jsx
@@ -1,10 +1,12 @@
 import Navbar from './components/Navbar'
 import Home from './components/Home'
+import About from './components/About'
 export default function Main() {
   return (
     <>
     <Navbar/>
     <Home/>
+    <About/>
     </>
   )
 }
