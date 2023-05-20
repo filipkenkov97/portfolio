95bfa3ea2e06c2b31d33fd3ffe0a8b22f4b58510
filipkenkov <filip.kenkov@miraclemill.com>
2023-05-20 18:58:51 +0200

    home: text changed to 2xl

--- app/components/Home.jsx
+++ app/components/Home.jsx
@@ -8,7 +8,7 @@ function About() {
       <div className="max-w-[1024px] w-full h-full mx-auto flex justify-center items-center">
         <div className="mx-auto">
             <h1 className="text-white">Hi, I am <span className="text-blue-800">Filip</span></h1>
-            <h1 className="text-white my-2 text-3xl">Fullstack Developer @ <span className="text-blue-800">Miracle Mill</span></h1>
+            <h1 className="text-white my-2 text-2xl">Fullstack Developer @ <span className="text-blue-800">Miracle Mill</span></h1>
             <p className="py-2 text-white max-w-[70%] m-auto">
             Graduate at Faculty of Computer Science and Engineering
             <br/>
