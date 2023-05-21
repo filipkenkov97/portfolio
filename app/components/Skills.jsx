ea58b832c5255a792739db2d526bc7913cc50655
filipkenkov <filip.kenkov@miraclemill.com>
2023-05-22 00:56:30 +0200

    small screen fix

--- app/components/Skills.jsx
+++ app/components/Skills.jsx
@@ -12,7 +12,7 @@ const About = () => {
   return (
     <div id="skills" className="w-screen h-screen items-center">
       <div className="my-auto max-w-[1240px] m-auto md:grid">
-        <div className="py-96 md:py-40">
+        <div className="py-96 my-96 md:py-40">
           <p className="text-2xl font-bold text-blue-800 uppercase py-4 sm:mx-2">Skills</p>
           <div className="grid grid-cols-2 lg:grid-cols-4 gap-8">
             <div className="p-4 shadow-2xl rounded-xl hover:scale-105 ease-in duration-300 hover:cursor-pointer">
